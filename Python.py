81ccdeebf407669f2e4b6a199cf4b9cadbad7c78
galien1 <galienshadowstriker@gmail.com>
2017-07-07 15:28:06 -0400

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,12 @@
+#Assigning variables
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+
+print caesar
+print praline
+print viking
+
+#You can fix text by using an escape backslash
+'This isn\'t flying this is falling with style!'
